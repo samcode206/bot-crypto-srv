393ab79a99eadab999bbdd1e83992574435a5d41
samcode206 <66322745+samcode206@users.noreply.github.com>
2021-06-30 04:40:07 -0700

    Update reqParser.js

--- worker/src/lib/reqParser.js
+++ worker/src/lib/reqParser.js
@@ -3,7 +3,7 @@ const opts = require("../config/options.js");
 // converts the fetched data to proper format 
 const parseResponse = (exchange) =>{
     if (exchange.status && exchange.status === -1) return null; 
-    console.log("fetched new prices at: " + Date.now()); 
+    console.log("fetched new prices at: " + new Date()); 
     const ask = parseFloat(exchange.ask)
     const bid = parseFloat(exchange.bid)
     return {
